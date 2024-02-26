0d3a30f3015c36f51d6822118c8a4d9bac9e8de6
GoyalDhruv <70637670+GoyalDhruv@users.noreply.github.com>
2024-02-26 06:22:01 +0000

    datatype summary

---
+++ Basics/datatype-summary.js
@@ -0,0 +1,21 @@
+// Primitive
+// 7 Type => String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// Reference (Non Primitive)
+// Array, Object, Function
+
+// Arrays
+let array=[1,2,3,4]
+
+// Object
+myobj={
+    name:'abc',
+    age:20,
+}
+
+// Function
+const myfunc=function(){
+    console.log("Hello World")
+}
+console.log(typeof(myfunc))
