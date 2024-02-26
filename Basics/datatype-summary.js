daa864c0a7952b000ab3189eab2dedf3496c0153
GoyalDhruv <70637670+GoyalDhruv@users.noreply.github.com>
2024-02-26 06:47:27 +0000

    file updated

--- Basics/datatype-summary.js
+++ Basics/datatype-summary.js
@@ -11,7 +11,7 @@ let array=[1,2,3,4]
 // Object
 myobj={
     name:'abc',
-    age:20,
+    age:20
 }
 
 // Function
@@ -19,3 +19,29 @@ const myfunc=function(){
     console.log("Hello World")
 }
 console.log(typeof(myfunc))
+
+// --------------------------------
+// Stack Memory (Primitive) {Copy is generated}
+
+let variable1="abc"
+let variable2=variable1
+variable2="def"
+
+console.log(variable2);
+console.log(variable1);
+
+
+
+// Heap Memory (Non-Primitive) {Reference of Original value}
+
+let userOne={
+    "email":"abc@gmail.com",
+    "upi":"abc@abc"
+}
+
+let userTwo=userOne
+
+userTwo.email='xyz@gmail.com'
+
+console.log(userOne.email);
+console.log(userTwo.email);
