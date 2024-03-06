b017cf5157c33ca57120c5f7c51930390db4c901
GoyalDhruv <70637670+GoyalDhruv@users.noreply.github.com>
2024-03-06 07:57:56 +0000

    json talk

---
+++ Basics_2/05_objects_part3.js
@@ -0,0 +1,17 @@
+const course={
+    coursename:'javascript',
+    price:999,
+    instructor:'abc'
+}
+
+const {instructor:ins} = course
+
+// console.log(instructor)
+console.log(ins)
+
+// JSON Format
+// {
+//     "name":'abc',
+//     "coursename":"js in hindi",
+//     "price":"free"
+// }
